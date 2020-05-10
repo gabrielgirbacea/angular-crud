b25a9d179abe97cff1a2b21752e35566cc9b8b79
Gabriel Girbacea <gabriel.girbacea@yahoo.com>
2020-05-10 14:55:39 +0300

    Added basic routing with lazy loading for feature modules

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,12 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
 
-
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "signup",
+    component: SignupComponent
+  },
+  {
+    path: "",
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)]
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
--- src/app/contact/contact-routing.module.ts
+++ src/app/contact/contact-routing.module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ContactEditComponent } from "./contact-edit/contact-edit.component";
+import { ContactListComponent } from "./contact-list/contact-list.component";
 
-
+const routes: Routes = [
+  {
+    path: "add-contact",
+    component: ContactEditComponent
+  },
+  {
+    path: "",
+    component: ContactListComponent
+  }
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)]
 })
-export class ContactRoutingModule { }
+export class ContactRoutingModule {}
