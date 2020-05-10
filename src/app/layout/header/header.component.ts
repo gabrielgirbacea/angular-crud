0c4a41a20cb825546753aadd7d9680408196ea1e
Gabriel Girbacea <gabriel.girbacea@yahoo.com>
2020-05-10 18:47:50 +0300

    Hide/Show navbar elements whether the user is logged in or not

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { AuthService } from "@services/auth.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.sass"]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  constructor(public authService: AuthService) {}
 
-  constructor() { }
+  isAuthenticated = false;
 
-  ngOnInit(): void {
+  logout(): void {
+    this.authService.logout();
   }
-
 }
