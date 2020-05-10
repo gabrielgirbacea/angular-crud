a9fac97abbff35ec15bc1572f17daf37e8932922
Gabriel Girbacea <gabriel.girbacea@yahoo.com>
2020-05-10 16:44:33 +0300

    Added responsive navigation with routing

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./header/header.component";
+import { MaterialModule } from "../material.module";
+import { RouterModule } from "@angular/router";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule, RouterModule, FlexLayoutModule],
   exports: [HeaderComponent]
 })
 export class LayoutModule {}
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { MatIconModule } from "@angular/material/icon";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTooltipModule } from "@angular/material/tooltip";
 
 @NgModule({
   declarations: [],
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule],
-  exports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule]
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule, MatIconModule, MatTooltipModule],
+  exports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule, MatIconModule, MatTooltipModule]
 })
 export class MaterialModule {}
